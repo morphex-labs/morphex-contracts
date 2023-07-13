9d664371a8cf1b0764b6daf21aa2d6581f79f268
daedboi <87483308+daedboi@users.noreply.github.com>
2023-07-13 17:51:25 +0300

    Add volume reward weeks

--- scripts/volume-rewards/volume.js
+++ scripts/volume-rewards/volume.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { ethers } = require('ethers');
 
-const filename = 'weeks/fantom/week15.csv'; // change this to the week you want to calculate
+const filename = 'weeks/fantom/week16.csv'; // change this to the week you want to calculate
 const outputFilename = 'output.json';
 
 const outputObj = {};
