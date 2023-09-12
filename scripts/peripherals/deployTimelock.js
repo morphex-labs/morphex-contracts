812a26e5762494189b3907125f4ab2db299b8bb0
daedboi <87483308+daedboi@users.noreply.github.com>
2023-09-12 20:01:37 +0300

    Update deployTimelock.js

--- scripts/peripherals/deployTimelock.js
+++ scripts/peripherals/deployTimelock.js
@@ -1,63 +1,21 @@
 const { deployContract, contractAt, sendTxn, getFrameSigner } = require("../shared/helpers")
 const { expandDecimals } = require("../../test/shared/utilities")
 
-const network = (process.env.HARDHAT_NETWORK || 'mainnet');
-
-async function getArbValues() {
-  const vault = await contractAt("Vault", "0x489ee077994B6658eAfA855C308275EAd8097C4A")
-  const tokenManager = { address: "0xddDc546e07f1374A07b270b7d863371e575EA96A" }
-  const glpManager = { address: "0x3963FfC9dff443c2A94f21b129D429891E32ec18" }
-  const rewardRouter = { address: "0xB95DB5B167D75e6d04227CfFFA61069348d271F5" }
-
-  const positionRouter = { address: "0xb87a436B93fFE9D75c5cFA7bAcFff96430b09868" }
-  const positionManager = { address: "0x75E42e6f01baf1D6022bEa862A28774a9f8a4A0C" }
-  const gmx = { address: "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a" }
-
-  return { vault, tokenManager, glpManager, rewardRouter, positionRouter, positionManager, gmx }
-}
-
-async function getAvaxValues() {
-  const vault = await contractAt("Vault", "0x9ab2De34A33fB459b538c43f251eB825645e8595")
-  const tokenManager = { address: "0x8b25Ba1cAEAFaB8e9926fabCfB6123782e3B4BC2" }
-  const glpManager = { address: "0xD152c7F25db7F4B95b7658323c5F33d176818EE4" }
-  const rewardRouter = { address: "0xB70B91CE0771d3f4c81D87660f71Da31d48eB3B3" }
-
-  const positionRouter = { address: "0xffF6D276Bc37c61A23f06410Dce4A400f66420f8" }
-  const positionManager = { address: "0xA21B83E579f4315951bA658654c371520BDcB866" }
-  const gmx = { address: "0x62edc0692BD897D2295872a9FFCac5425011c661" }
-
-  return { vault, tokenManager, glpManager, rewardRouter, positionRouter, positionManager, gmx }
-}
-
-async function getValues() {
-  if (network === "arbitrum") {
-    return getArbValues()
-  }
-
-  if (network === "avax") {
-    return getAvaxValues()
-  }
-}
-
 async function main() {
   // const signer = await getFrameSigner()
 
   const admin = "0xB1dD2Fdb023cB54b7cc2a0f5D9e8d47a9F7723ce"
-  const buffer = 24 * 60 * 60 // 48 * 60 * 60 --> 24 hours
+  const buffer = 0 * 60 * 60 
   const maxTokenSupply = expandDecimals("10000000", 18)
 
   // const { vault, tokenManager, glpManager, rewardRouter, positionRouter, positionManager, gmx } = await getValues()
 
-  const vault = await contractAt("Vault", "0xec8d8D4b215727f3476FF0ab41c406FA99b4272C")
   const tokenManager = { address: "0xE02Fb5C70aF32F80Aa7F9E8775FE7F12550348ec" }
   const glpManager = { address: "0x9fAc7b75f367d5B35a6D6D0a09572eFcC3D406C5" }
   const rewardRouter = { address: "0x49A97680938B4F1f73816d1B70C3Ab801FAd124B" }
 
   const positionRouter = { address: "0x927F9c03d1Ac6e2630d31E614F226b5Ed028d443" }
   const positionManager = { address: "0x2ace8F6Cc1ce4813Bd2D3AcE550ac95810855C40" }
-
-  const gmx = { address: "0x66eEd5FF1701E6ed8470DC391F05e27B1d0657eb" }
-
 
   const mintReceiver = tokenManager
 
