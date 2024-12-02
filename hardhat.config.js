db79d4b641aaa2fb55e1a78da135256b7085a1d3
daedboi <87483308+daedboi@users.noreply.github.com>
2024-12-02 15:43:42 +0200

    token config updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -54,7 +54,7 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: "mode",
+  defaultNetwork: "base",
   networks: {
     localhost: {
       timeout: 120000
