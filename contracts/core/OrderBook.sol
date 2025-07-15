ea9d16b9e5f0f2be04daa8dd01c0bb81c99375a9
daedboi <87483308+daedboi@users.noreply.github.com>
2025-07-15 23:41:23 +0300

    fix: remove "payable" for _feeReceiver

--- contracts/core/OrderBook.sol
+++ contracts/core/OrderBook.sol
@@ -731,7 +731,7 @@ contract OrderBook is ReentrancyGuard, IOrderBook {
         );
     }
 
-    function executeIncreaseOrder(address _address, uint256 _orderIndex, address payable _feeReceiver) override external nonReentrant {
+    function executeIncreaseOrder(address _address, uint256 _orderIndex, address _feeReceiver) override external nonReentrant {
         IncreaseOrder memory order = increaseOrders[_address][_orderIndex];
         require(order.account != address(0), "OrderBook: non-existent order");
 
@@ -843,7 +843,7 @@ contract OrderBook is ReentrancyGuard, IOrderBook {
         );
     }
 
-    function executeDecreaseOrder(address _address, uint256 _orderIndex, address payable _feeReceiver) override external nonReentrant {
+    function executeDecreaseOrder(address _address, uint256 _orderIndex, address _feeReceiver) override external nonReentrant {
         DecreaseOrder memory order = decreaseOrders[_address][_orderIndex];
         require(order.account != address(0), "OrderBook: non-existent order");
 
